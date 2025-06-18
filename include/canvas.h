2c65e442af6457ecaac03ee8982fcaf8938b84ae
Gayasha Sandeepa <e22353@eng.pdn.ac.lk>
2025-06-18 12:40:15 +0530

    Update canvas.h
    Updating the function prototypes

--- include/canvas.h
+++ include/canvas.h
@@ -12,5 +12,7 @@ canvas_t * create_canvas(int width, int height);
 void remove_canvas(canvas_t *canvas);
 float get_pixel(canvas_t *canvas, int x, int y);
 void set_pixel(canvas_t *canvas, int x, int y, float value);
+void set_pixel_f(canvas_t *canvas, float x, float y, float value);
+void draw_line_f(canvas_t *canvas, float x0, float y0, float x1, float y1, float thickness);
 
 #endif
