0e4616cb6b84aa465703e626efd2999065756c68
Gayasha Sandeepa <e22353@eng.pdn.ac.lk>
2025-07-01 21:18:53 +0530

    Update math3d.h

--- include/math3d.h
+++ include/math3d.h
@@ -1,6 +1,20 @@
 #ifndef MATH3D_H
 #define MATH3D_H
 
+typedef struct {
+    float x;
+    float y;
+    float z;
 
+    float r;
+    float theta;
+    float phi;
+} vec3_t;
+
+void update_spherical_coordinates(vec3_t *v);
+void update_cartesian_coordinates(vec3_t *v);
+vec3_t vec3_from_spherical(float r, float theta, float phi);
+vec3_t vec3_normalize_fast(vec3_t v);
+vec3_t vec3_slerp(vec3_t a, vec3_t b, float t);
 
 #endif
