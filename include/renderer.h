e4ff062d9bda09f0b989f03844f34246063f144f
Gayasha Sandeepa <e22353@eng.pdn.ac.lk>
2025-07-04 12:26:08 +0530

    Update renderer.h

--- include/renderer.h
+++ include/renderer.h
@@ -1,6 +1,9 @@
 #ifndef RENDERER_H
 #define RENDERER_H
 
-// your code here...
+#include "canvas.h"
+#include "math3d.h"
+
+void render_wireframe(canvas_t *canvas, vec3_t *vertices, int vertex_count, int edges[][2], int edge_count, mat4_t model, mat4_t proj);
 
 #endif
