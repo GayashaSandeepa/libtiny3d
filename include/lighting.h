75775f4dd2e8fce0b8bd5857df17d93989d897ba
Gayasha Sandeepa <e22353@eng.pdn.ac.lk>
2025-07-07 14:22:02 +0530

    Update lighting.h

--- include/lighting.h
+++ include/lighting.h
@@ -1,6 +1,10 @@
 #ifndef LIGHTING_H
 #define LIGHTING_H
 
+#include "math3d.h"
 
+// Calculate Lambert lighting intensity for an edge defined by start and end points,
+// given a light direction vector. Returns value between 0 and 1.
+float lambert_intensity(vec3_t start, vec3_t end, vec3_t light_dir);
 
-#endif
+#endif // LIGHTING_H
